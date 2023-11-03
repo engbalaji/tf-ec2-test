e28fa11fbe9ca660eb755d41e940fb32c3e054db
Balaji Mariyappan <engbalaji@users.noreply.github.com>
2023-11-03 14:14:38 -0400

    Update the issue

--- myproject2/main.tf
+++ myproject2/main.tf
@@ -5,10 +5,8 @@ provider "aws" {
 resource "aws_instance" "example1" {
   ami           = var.ami_id
   instance_type = var.instance_type
-
   subnet_id    = var.subnet1_id
   vpc_security_group_ids = [var.security_group1_id]
-
   tags = {
     Name = var.instance1_name
   }
@@ -17,10 +15,8 @@ resource "aws_instance" "example1" {
 resource "aws_instance" "example2" {
   ami           = var.ami_id
   instance_type = var.instance_type
-
   subnet_id    = var.subnet2_id
   vpc_security_group_ids = [var.security_group2_id]
-
   tags = {
     Name = var.instance2_name
   }
