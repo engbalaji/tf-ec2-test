6b3db4d51c3ecff58b5b063620717756af922453
Balaji Mariyappan <engbalaji@users.noreply.github.com>
2023-11-03 14:21:56 -0400

    Tag uupdate

--- myproject2/main.tf
+++ myproject2/main.tf
@@ -5,8 +5,10 @@ provider "aws" {
 resource "aws_instance" "example1" {
   ami           = var.ami_id
   instance_type = var.instance_type
+
   subnet_id    = var.subnet1_id
   vpc_security_group_ids = [var.security_group1_id]
+  
   tags = {
     Name = var.instance1_name
   }
@@ -15,8 +17,10 @@ resource "aws_instance" "example1" {
 resource "aws_instance" "example2" {
   ami           = var.ami_id
   instance_type = var.instance_type
+
   subnet_id    = var.subnet2_id
   vpc_security_group_ids = [var.security_group2_id]
+
   tags = {
     Name = var.instance2_name
   }
