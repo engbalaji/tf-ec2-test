7b3ebd7285c11fb0eee8e72aa2186878eaae2fa2
Balaji Mariyappan <engbalaji@users.noreply.github.com>
2024-01-26 14:01:12 -0500

    new folder updated with files

--- myproject4/main.tf
+++ myproject4/main.tf
@@ -1,8 +1,56 @@
 provider "aws" {
     // Configure the AWS provider here
-    region = "us-east-1"
+    region = var.aws_region
 }
 
 ####################################
+# VPC and subnets
+####################################
+
+data "aws_vpc" "vpc" {
+    // Configure the VPC here
+    id = "var.vpc_id"
+}
+
+data "aws_subnet" "subnet1" {
+    // Configure the subnet here
+    id = "var.subnet1_id" 
+}
+
+####################################
+# Create an AWS EC2 instance
+####################################
+
+resource "aws_instance" "web" {
+    // Configure the AWS instance here
+    ami = "var.ami_id"
+    instance_type = "var.instance_type"
+    key_name = "var.my-key-pair"
+    vpc_security_group_ids = ["var.security_group1_id"]
+    tags = {
+        Name = "terraform-example"
+    }
+}
+
+####################################
+# Create an EBS volume and attach it to the EC2 instance
+####################################
+
+resource "aws_ebs_volume" "ebs_volume" {
+    // Configure the EBS volume here
+    availability_zone = "us-east-1a"
+    size = 33
+    type = "gp2"
+    tags = {
+        Name = "terraform-example"
+    }
+}
+
+resource "aws_volume_attachment" "ebs_attachment" {
+    // Configure the EBS volume attachment here
+    instance_id = aws_instance.web.id
+    volume_id = aws_ebs_volume.ebs_volume.id
+    device_name = "/dev/sdh"
+}
 
 ####################################
---
+++ myproject4/variable.tf
@@ -0,0 +1,34 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+variable "ami_id" {
+  default = "ami-0a3c3a20c09d6f377"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "subnet1_id" {
+  default = "subnet-0f1e42859f3ad43c3"
+}
+variable "subnet2_id" {
+  default = "subnet-0e0420f94c95acd22"
+}
+variable "security_group1_id" {
+  default = "sg-06bc9cdd724e815b1"
+}
+variable "security_group2_id" {
+  default = "sg-048baa2ebfebb7aae"
+}
+variable "instance1_name" {
+  default = "PEPOC1"
+}
+variable "instance2_name" {
+  default = "PEPOC2"
+}
+variable "my-key-pair" {
+  default = "Balaji Mariyappan"
+}
+variable "vpc_id" {
+  default = "vpc-b30658d4"
+}
+
