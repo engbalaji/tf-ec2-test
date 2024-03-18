0cb6cbb958f465e9886127fa79ab2a4c81782093
balaji mariyappan <engbalaji@users.noreply.github.com>
2024-03-18 11:58:38 -0400

    With infracost changes

--- example.tf
+++ example.tf
@@ -1,20 +1,35 @@
-
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region = "${var.region}"
+  region = "us-east-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id = true
+  #access_key = "mock_access_key"
+  #secret_key = "mock_secret_key"
 }
 
-resource "aws_instance" "example" {
-  ami = "${lookup(var.amis, var.region)}"
-  instance_type = "t2.micro"
+resource "aws_instance" "my_web_app" {
+  ami = "ami-005e54dee72cc1d00"
+
+  instance_type = "m3.xlarge" # <<<<<<<<<< Try changing this to m5.xlarge to compare the costs
+
+  tags = {
+    Environment = "production"
+    Service = "web-app"
+  }
+
+  root_block_device {
+    volume_size = 1000 # <<<<<<<<<< Try adding volume_type="gp3" to compare costs
+  }
 }
 
-resource "aws_eip" "ip" {
-  instance = "${aws_instance.example.id}"
-  depends_on = ["aws_instance.example"]
-}
+resource "aws_lambda_function" "my_hello_world" {
+  runtime = "nodejs12.x"
+  handler = "exports.test"
+  image_uri = "test"
+  function_name = "test"
+  role = "arn:aws:ec2:us-east-1:123123123123:instance/i-1231231231"
 
-output "pub_ip" {
-   value = "${aws_instance.example.public_ip}"
+  memory_size = 512
+  tags = {
+    Environment = "Prod"
+  }
 }
